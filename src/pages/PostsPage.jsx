95279ed0fbd156fb2df7eaa98a388e07e5da7028
MattiaPlacido <placido.mattia@gmail.com>
2024-12-10 16:20:01 +0100

    Estetica

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react";
 
+const initialPostData = {
+  title: "Default title",
+  content: "Default content",
+  image: "https://via.placeholder.com/600/771796",
+  category: "Sport",
+  published: true,
+  id: -1,
+};
+
 export default function PostsPage() {
-  const [postData, setPostData] = useState([]);
+  const [postData, setPostData] = useState([initialPostData]);
 
   const getData = () => {
     fetch("http://localhost:3000")
       .then((res) => res.json())
       .then((data) => {
-        setPostData(data);
+        setPostData(data.posts);
       })
       .catch((error) => console.error("Errore nella richiesta SHOW :", error));
   };
@@ -15,11 +24,21 @@ export default function PostsPage() {
   useEffect(getData, []);
 
   return (
-    <main className="container py-5 text-center">
-      <h2>Lista dei post :</h2>
-      {postData.map((post) => (
-        <p>{post.id}</p>
-      ))}
+    <main className="container py-5">
+      <div className="row row-cols-2">
+        {postData.map((post) => (
+          <div className="card col-6" key={post.key}>
+            <img
+              src={post.image || "https://via.placeholder.com/600/771796"}
+              className="card-img-top mx-auto w-100 "
+            />
+            <div className="card-body">
+              <h5 className="card-title">{post.title}</h5>
+              <p className="card-text fs-6">{post.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
