a9662c60967e30b3175675d5aae7aac9a64c3f39
MattiaPlacido <placido.mattia@gmail.com>
2024-12-10 15:34:17 +0100

    Estetica footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="bg-dark py-4">
+      <div className="container">
+        <a href="" className="text-decoration-none text-light mx-2">
+          Chi siamo
+        </a>
+        <a href="" className="text-decoration-none text-light mx-2">
+          Instagram
+        </a>
+        <a href="" className="text-decoration-none text-light mx-2">
+          Youtube
+        </a>
+        <a href="" className="text-decoration-none text-light mx-2">
+          Twitter
+        </a>
+        <a href="" className="text-decoration-none text-light mx-2">
+          Gmail
+        </a>
+      </div>
     </footer>
   );
 }
