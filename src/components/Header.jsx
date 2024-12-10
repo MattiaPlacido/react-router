7125f9693be786002e442d2fc4b041728237a419
MattiaPlacido <placido.mattia@gmail.com>
2024-12-10 15:36:53 +0100

    Estetica header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
-    <header>
-      <p>Header</p>
+    <header className="bg-dark py-4">
+      <div className="container">
+        <a href="" className="text-decoration-none text-light mx-2">
+          About
+        </a>
+      </div>
     </header>
   );
 }
