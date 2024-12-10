b88b052a1266515d2633cca63adaf65398666209
MattiaPlacido <placido.mattia@gmail.com>
2024-12-10 14:09:45 +0100

    Inizializzazione repo partendo dal boilerPlate visto a lezione

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// PAGINE
+import HomePage from "./pages/HomePage";
+// LAYOUT
+import DefaultLayout from "./layouts/DefaultLayout";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/about" element={<h2>AboutPage</h2>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  <h2>HomePage</h2>;
+}
